a71a398cf60e5a168568dbb7cfebb0d95e144d8e
Applesaurus <hennervate@tutanota.com>
2018-11-24 20:08:44 +0000

    Update cryptcontrol.py

--- cryptcontrol.py
+++ cryptcontrol.py
@@ -1,8 +1,10 @@
 import getopt, sys, re
 
+
 def main():
-    try:  #make sure that no nonexistent options are specified
-        opts, args = getopt.getopt(sys.argv[1:], "hlua:g:e:t:m:", ["help", "list", "update", "attacks", "group", "exclude", "time", "memory"])
+    try:  # make sure that no nonexistent options are specified
+        opts, args = getopt.getopt(sys.argv[1:], "hlua:g:e:t:m:",
+                                   ["help", "list", "update", "attacks", "group", "exclude", "time", "memory"])
     except getopt.GetoptError as err:
         # if nonexistent options specified, print help information and exit
         usage()
@@ -17,21 +19,22 @@ def main():
             usage()
             sys.exit()
         elif opt in ('-l', '--list'):
-            #call update
+            # call update
             if args:
-               #listthings(args)
+                # listthings(args)
             else:
-               #listthings
+                #listthings()
+        # listthings
         elif opt in ('-u', '--update'):
-            #call update
-        elif opt in ('-a', '--attack', '-g', '-group'): 
-            attackArguments = re.split(',',args) #multiple arguments for the same opt should be comma seperated; split them into an array
-            for i in range (0 len(attackArguments):
-                   if opt in ('-a', '--attack'):
-                       objList[i] = attack.getAttackObj(attackArguments[i])
-                   else: groups = true
+        # call update
+        elif opt in ('-a', '--attack', '-g', '-group'):
+            attackArguments = re.split(',',args)  # multiple arguments for the same opt should be comma seperated; split them into an array
+            for i in range(0, len(attackArguments)):
+                if opt in ('-a', '--attack'):
+                    objList[i] = attack.getAttackObj(attackArguments[i])
+                else: groups = true
         elif opt in ('-e', '--exclusions'):
-            exclusionList = re.split(',',args)
+            exclusionList = re.split(',', args)
         elif opt in ('-t', '--time'):
             time = args
         elif opt in ('-m', '--memory'):
@@ -39,10 +42,10 @@ def main():
         else:
             usage()
             sys.exit(2)
-     
-    if groups:
-        objList = Attack.listFromGroups(attackArguments, exclusionList)
-    if objList:                   
-        #if objList is either a list of groups are a list of attacks, -a or -g must have been specified indicating that an attack should run
-        attackrunner(objList,time,memory,exclusions)
+
+        if groups:
+                objList = Attack.listFromGroups(attackArguments, exclusionList)
+        if objList:
+            # if objList is either a list of groups are a list of attacks, -a or -g must have been specified indicating that an attack should run
+                attackrunner(objList, time, memory, exclusions)
 
