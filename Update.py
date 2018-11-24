1654b1b751a9d06a0a4eaca4dabc56388946ad9a
wmharri <39197376+wmharri@users.noreply.github.com>
2018-11-24 16:52:03 -0600

    Update Update.py

--- Update.py
+++ Update.py
@@ -11,7 +11,7 @@ class Update:
 
     def loadModules(self, dirname):
         moduleList = []
-        for importer, package_name, _ in pkgutil.iter_modules([dirname]):
+        for importer, package_name, _ in pkgutil.iter_modules([dirname]): # The _ is a variable commonly applied to unused values
             full_package_name = '%s.%s' % (dirname, package_name)
             if full_package_name not in sys.modules:
                 module = importer.find_module(package_name).load_module(full_package_name)
