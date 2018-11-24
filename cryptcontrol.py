67eb63d34324aabc44ee9eca9c66ba9b7432d10e
Applesaurus <hennervate@tutanota.com>
2018-11-24 15:49:34 +0000

    Update cryptcontrol.py

--- cryptcontrol.py
+++ cryptcontrol.py
@@ -8,8 +8,7 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hlua:g:e:t:m:", ["help", "list", "update", "attacks", "group", "exclude", "time", "memory"])
     except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
+        # print help information and exit
         usage()
         sys.exit(2)
     output = None
