99d63c2c43db8003a627227949fbd595739b08b8
wmharri <39197376+wmharri@users.noreply.github.com>
2018-11-24 16:14:14 -0600

    Update modInverse.py
    Comments added

--- modInverse.py
+++ modInverse.py
@@ -1,3 +1,18 @@
+## Written by Michael Harrison
+## November 24, 2018
+##
+## The functions contained within this file are written
+## to solve the Diophantine equation ax + by = g,
+## where g is the greatest common factor of a and b.
+## In the case g = 1, this provides a means for calculating
+## the multiplicative inverse of a, modulo b (and vice-versa).
+
+## inversionGCD implements the Euclidean algorithm on two integers.
+## Unlike the gcd function included with most programming languages,
+## inversionGCD returns the coefficients and remainders for each step;
+## this information is needed by modInverter.
+## @input firstInt, secondInt
+## @output remainder, coeff
 def inversionGCD(firstInt, secondInt):
     if abs(firstInt) == abs(secondInt):
         return [0], [firstInt//secondInt]
@@ -25,6 +40,15 @@ def inversionGCD(firstInt, secondInt):
     return remainder, coeff
 #    return firstInv%secondInt, secondInv%firstInt
 
+## modInverter uses the arrays from inversionGCD
+## to express each value of remainder as a linear combination
+## of the inputs to inversionGCD, down to the final remainder,
+## which is the greatest common factor of inversionGCD's inputs.
+## The remainder array is also used to terminate the algorithm.
+## The values from the coeff array are used to determine
+## the coefficients for this linear combination.
+## @input remainder, coeff
+## @output divCoeffNew, quotCoeffNew
 def modInverter(remainder, coeff):
     if(remainder[0] == 0):
         return 0, 1
