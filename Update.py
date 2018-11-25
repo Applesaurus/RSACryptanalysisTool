e76af6463e2440334e07b440cbe2b2bb7c013634
Applesaurus <hennervate@tutanota.com>
2018-11-25 23:06:04 +0000

    Update Update.py

--- Update.py
+++ Update.py
@@ -2,28 +2,32 @@ import os, pkgutil, sys, json
 
 ## Written by Jackie Bowers
 
-class Update:
+class update:
 
-    attackdir = ""
+    attackDir = "/Users/jackie/Desktop/pythontest"
 
     def __init__(self, directory):
-        self.attackdir = directory
+        self.attackDir = directory
 
     def loadModules(self, dirname):
         moduleList = []
-        for importer, package_name, _ in pkgutil.iter_modules([dirname]): # The _ is a variable commonly applied to unused values
+        for importer, package_name, _ in pkgutil.iter_modules([dirname]):
             full_package_name = '%s.%s' % (dirname, package_name)
+            print(full_package_name)
+            print(importer)
             if full_package_name not in sys.modules:
                 module = importer.find_module(package_name).load_module(full_package_name)
                 moduleList.append(module)
         return moduleList
 
     def runUpdate(self):
-        moduleList = self.loadModules(self.attackdir)
+        moduleList = self.loadModules(self.attackDir)
         identifier = {}
         for module in moduleList:
             try:
                 identifier = module.identify()
+                #identifier['module'] = '%s' % module
+                #print(identifier)
                 with open('attackData.txt', 'w') as file:
                     json.dump(identifier, file)
             except:
@@ -35,4 +39,4 @@ class Update:
             with open('AttackData.txt') as json_file:
                 attackData = json.load(json_file)
                 for i in attackData['attack']:
-                    print(p['attackName'])
+                    print(i['attackName'])
