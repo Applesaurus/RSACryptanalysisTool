ff5ec9a7cc0f868ee865fa51daca97bceab2a622
Applesaurus <hennervate@tutanota.com>
2018-11-25 23:06:34 +0000

    Update Update.py

--- Update.py
+++ Update.py
@@ -13,8 +13,6 @@ class update:
         moduleList = []
         for importer, package_name, _ in pkgutil.iter_modules([dirname]):
             full_package_name = '%s.%s' % (dirname, package_name)
-            print(full_package_name)
-            print(importer)
             if full_package_name not in sys.modules:
                 module = importer.find_module(package_name).load_module(full_package_name)
                 moduleList.append(module)
