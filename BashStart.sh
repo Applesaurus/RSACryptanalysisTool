097d5f49494daa4c9ca5a48ca037d9cd75b629c2
Applesaurus <hennervate@tutanota.com>
2018-11-26 17:41:37 +0000

    Update BashStart.sh

--- BashStart.sh
+++ BashStart.sh
@@ -3,6 +3,7 @@
 pyCryptControl="/cryptcontrol.py"
 
 dirPath="$( cd "$( dirname "{BASH_SOURCE[0]}" )" && pwd )"
+#replace dirname with the directory where cryptcontrol.py is located
 dirPath=$dirPath$pyCryptControl
 
 python3 "$dirPath" "$@"
