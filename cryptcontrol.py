385ec0bd853a05541540d42225d2520394b0e4f8
Applesaurus <hennervate@tutanota.com>
2018-11-26 18:21:11 +0000

    Update cryptcontrol.py

--- cryptcontrol.py
+++ cryptcontrol.py
@@ -49,7 +49,6 @@ def main():
         elif opt in ('-m', '--memory'): #specify max memory each attack allowed to use
             memory = args
         elif opt in ('-p', '--parameters'): #specify parameters in order: publicexponent, modulus, cyphertext, plaintext
-            #handle the parameters
             #if re.match('.txt', args):
                 #read json
             parameters = re.split(',', args)
@@ -72,7 +71,31 @@ def main():
 
 
 def usage():
-    print('help goes here')
+    print('To run this program from the command line, run either cryptcontrol.py with python or run BashStart.\n\n'
+          'All arguments follow their options.\n'+
+          'usage for each flag is as follows:\n' +
+          'To specify an attack or multiple attacks to run use:\n\n' +
+          '-a <attack1>,<attack2>,ect\n\n' +
+          'All attacks must also take parameters to run.\n'+
+          'Parameters are passed as follows:\n\n' +
+          '-p <publicexponent>,<modulus>,<cyphertext>,<plaintext>\n\n' +
+          'Although many attacks may not require every parameter as an argument,\n' +
+          'some attacks may require multiple sets of inputs, in which case the subsequent sets may\n' +
+          'immediately follow the previous sets or [TODO] all sets may be listed seperatedly by a new line in a .txt file\n' +
+          'attacks may be specified by group as follows:\n\n' +
+          '-g <group1>,<group2>,ect\n\n' +
+          'groups must also take parameters. Additionally, attacks belonging to selected groups can be excluded with -e:\n\n' +
+          '-e <excludedAttack1>,<excludedAttack2>,ect\n\n' +
+          '-u updates the persistent list of attacks in the attack directory and their properties and takes no arguments\n\n'
+          '-l both updates the list of attacks in the attack directory and lists them. It may take no arguments or\n' +
+          '[TODO] may take a group as an argument and list only the members of that group\n\n' +
+          '-t and -m may be specified when using -g or -a to provide time and memory limits for each attack\n' +
+          'arguments for -t must be in the format of either <amount>d , <amount>h, <amount>m, or <amount>s\n' +
+          'which specifies the days, hours, minutes, or seconds respectively that each attack should be allowed to run\n' +
+          '[TODO] arguments for -m must be in the format of either <amount>TiB, <amount>GiB, <amount>MiB, or  <amount>KiB\n\n' +
+          '-s allows the attack directory to be changed to a directory specified. It takes a single filepath as an argument')
+
+
 
 if __name__ == "__main__":
     main()
